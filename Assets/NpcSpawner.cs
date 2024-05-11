720399bcedd0a6609eb504610719ae851e864711
Mert Ateş Topuz <m.atestopuz@gmail.com>
2024-05-11 05:46:51 +0300

    Commit 4

--- Assets/NpcSpawner.cs
+++ Assets/NpcSpawner.cs
@@ -4,16 +4,43 @@ using UnityEngine;
 
 public class NpcSpawner : MonoBehaviour
 {
-    public GameObject npcPrefab;
+    public static NpcSpawner instance;
+
+    public GameObject npcPrefab1,npcPrefab2,npcPrefab3;
     public GameObject spawnedNpc;
     public bool isNpcSpawned;
     public int value;
+    public int npcCounter;
+    private GameObject mainNpcPrefab;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
     
     void Update()
     {
         if(isNpcSpawned == false)
         {
-            spawnedNpc = Instantiate(npcPrefab,transform.position,transform.rotation);
+            switch (npcCounter)
+            {
+                case 0:
+                    mainNpcPrefab = npcPrefab1;
+                    break;
+                
+                case 1:
+                    mainNpcPrefab = npcPrefab2;
+                    break;
+                
+                case 2:
+                    mainNpcPrefab = npcPrefab3;
+                    break;
+            }
+
+            spawnedNpc = Instantiate( mainNpcPrefab,transform.position,transform.rotation);
             isNpcSpawned = true;
         }
 
